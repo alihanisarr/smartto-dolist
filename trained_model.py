e311aaef236ca4a32ef3373a58e7ba923097be94
alihanisarr <aliha.nra619@gmail.com>
2025-07-23 15:10:12 -0700

    Update trained_model.py

--- trained_model.py
+++ trained_model.py
@@ -3,22 +3,23 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 import joblib
 
-
+# Load the task data
 dataFrame = pd.read_csv("task_data.csv")
 
-# Feature engineering
+# Number of words in the task name being converted to a feature
 dataFrame["no_of_words"] = dataFrame["task_name"].apply(lambda x: len(x.split()))
+
+# Priority and task type being converted to numbers
 dataFrame["priority_type"] = LabelEncoder().fit_transform(dataFrame["priority"])
 dataFrame["task_type"] = LabelEncoder().fit_transform(dataFrame["type_of_task"])
 
-
+# Identifying features and target variable
 X = dataFrame[["no_of_words", "priority_type", "task_type"]]
 y = dataFrame.time_required_minutes
 
-
+# Training the model 
 trained_task_model = RandomForestRegressor(random_state=1)
 trained_task_model.fit(X, y)
-
 
 joblib.dump(trained_task_model, "final_trained_model.pkl")
 
