b1512c0b5c63d90c59e9b04ee1fad3e296a79472
alihanisarr <aliha.nra619@gmail.com>
2025-07-27 20:55:33 -0700

    script.js

--- script.js
+++ script.js
@@ -24,42 +24,49 @@ async function addTask() {
   const li = document.createElement("li");
   li.textContent = `${taskText} (Estimated time required: ${predictedTime} minutes)`;
 
-    // Toggle 'completed' on CSS so user can mark tasks done or undone
+    // This toggles the CSS class 'completed' to show that a task is done
     li.addEventListener("click", function () {
       li.classList.toggle("completed");
     });
 
-    // Create a delete button with each task item
+    // Create a delete button 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     deleteBtn.classList.add("delete-btn");
+   
     deleteBtn.addEventListener("click", function (e) { // To prevent event propagation : parent events like toggling
       e.stopPropagation();
       li.remove();
     });
 
-    // Append delete button inside li and add the task to the task list
+    // Append delete button inside li 
     li.appendChild(deleteBtn);
+    
+    // Add both task + delete button to the task list in DOM
     document.getElementById("taskList").appendChild(li);
     input.value = ""; // Clear input block after adding a task item
   }
 
 // Function to get predicted time from backend
   async function getPrediction(taskText, priority, taskType) {
-    const res = await fetch("http://localhost:5001/predict", {  // Task data sent as JSON
+    
+      // Sending POST request to server, this request includes task data as JSON
+      const res = await fetch("http://localhost:5001/predict", {  
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        task_name: taskText,
-        priority: priority,      
-        type_of_task: taskType      
+        task_name: taskText,  // Task description
+        priority: priority,  // Numeric value    
+        type_of_task: taskType    // Numeric value
       }),
     });
-    if (!res.ok) {
-      throw new Error('Network response was not ok');
-    }
-    
-    const data = await res.json();
-    return data.predicted_minutes_required;
-  }
-  
+
+    // 
+   if (!res.ok) {
+  console.warn("Server responded with error, now giving default value.");
+  return "Unknown/0"; 
+}
+
+const data = await res.json();
+      
+return data.predicted_minutes_required;
